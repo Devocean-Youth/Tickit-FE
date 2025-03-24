61ee5048513b7ea6e603ed26454dfaeeff953c1b
clice lee <cliceleee@gmail.com>
2025-03-24 16:27:25 +0900

    Feat: 홈 화면 컴포넌트화, Header, CardSection 개발

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import Header from '../components/HomePage/Header';
+import Footer from '../components/HomePage/Footer';
 
 export const metadata: Metadata = {
-  title: 'Tickit - 행사 티켓 플랫폼',
+  title: '한 장의 티켓에 현장을 담다-티킷',
   description: '다양한 행사를 열고 티켓을 구매할 수 있는 웹사이트',
 };
 
@@ -14,8 +16,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
+        <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
-} 
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import CardSection from '../components/HomePage/CardSection';
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-      <p className="mt-4 text-lg">Tickit - 행사 티켓 플랫폼에 오신 것을 환영합니다!</p>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#101012]">
+      <CardSection />
     </div>
   );
-} 
+}
