392a1077fa3d0a73ebf1af6036c1dba514e5b860
clice lee <cliceleee@gmail.com>
2025-04-10 18:21:47 +0900

    Feat: 이벤트 생성 시 몇 스텝을 보여주는 StepIndicator 개발

--- src/app/event-create/page.tsx
+++ src/app/event-create/page.tsx
@@ -1,18 +1,54 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from 'react';
 import Step1Channel from '../../components/CreateEventPage/Step1Channel';
 import Step2EventDetail from '@/components/CreateEventPage/Step2EventDetail';
 import Step3Category from '@/components/CreateEventPage/Step3Category';
+import StepIndicator from '@/components/CreateEventPage/StepIndicator';
+
 const EventCreatePage: React.FC = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <Step1Channel />;
+      case 2:
+        return <Step2EventDetail />;
+      case 3:
+        return <Step3Category />;
+      default:
+        return null;
+    }
+  };
+
+  const handleNextStep = () => {
+    if (currentStep < 3) setCurrentStep(currentStep + 1);
+  };
+
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>이벤트 생성</h1>
-      <Step1Channel />
-      <h1>이벤트 상세</h1>
-      <Step2EventDetail />
-      <h1>이벤트 카테고리</h1>
-      <Step3Category />
+    <div style={{ display: 'flex' }}>
+      <StepIndicator currentStep={currentStep} />
+      <div style={{ flex: 1, padding: '20px' }}>
+        <h1>이벤트 생성</h1>
+        {renderStep()}
+        {currentStep < 3 && (
+          <button
+            onClick={handleNextStep}
+            style={{
+              marginTop: '20px',
+              padding: '10px 20px',
+              backgroundColor: '#1B56FD',
+              color: '#fff',
+              border: 'none',
+              borderRadius: '8px',
+              cursor: 'pointer',
+            }}
+          >
+            다음
+          </button>
+        )}
+      </div>
     </div>
   );
 };
