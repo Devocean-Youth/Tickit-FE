e0ed82d4d6a7054fc0326dcf6ec60f8e3c8d7c6c
clice lee <cliceleee@gmail.com>
2025-04-10 18:28:13 +0900

    Fix: 홈 화면에서 이벤트 생성버튼 누르면 해당 페이지로 이동되도록 수정

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -5,7 +5,6 @@ const Header = () => {
     <header className="flex justify-between items-center p-4 bg-[#101012] text-white">
       <h1 className="text-xl font-bold">TickIt</h1>
       <div className="flex gap-4">
-        <a href="/event-create" className="text-sm">이벤트 만들기</a>
         <a href="/login" className="text-sm">로그인</a>
       </div>
     </header>
